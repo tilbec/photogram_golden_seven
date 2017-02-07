51536b69c6423d9b8999f9931c822edbe7fdcd07
tilbec <tilbec@users.noreply.github.com>
2017-02-06 19:13:40 -0600

    Can't 2 tests

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,48 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @id = params[:id]
+    @photo = Photo.find(@id)
+  end
+
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("/photos")
+  end
+
+  def destroy
+    id = params[:id]
+    photo = Photo.find(id)
+    photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def edit_form
+    id = params[:id]
+    @photo = Photo.find(id)
+  end
+
+  def update_row
+    @id = params[:id]
+    new_caption = params[:the_caption]
+    new_source = params[:the_source]
+
+    @photo = Photo.find(@id)
+    @photo.source = new_source
+    @photo.caption = new_caption
+
+    @photo.save
+
+    redirect_to action: "show", id: @id
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
+  # Routes to CREATE photos
   get("/", { :controller => "photos", :action => "index" })
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
+  get("/create_photo",{ :controller => "photos", :action => "create_row" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  # Route to DELETE photos
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  get("/photos/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+  # Routes to UPDATE
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
 end
